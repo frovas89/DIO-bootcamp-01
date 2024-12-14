db22096b59d31b65ac82542c248f3a6b934c3b1f
frovas89 <frovas@gmail.com>
2024-12-14 18:15:33 -0300

    Entidade

---
+++ dio-avengers-api/src/main/kotlin/br/com/frovas/avengers/resource/avenger/AvengerEntity.kt
@@ -0,0 +1,34 @@
+package br.com.frovas.avengers.resource.avenger
+
+import br.com.frovas.avengers.domain.avenger.Avenger
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import org.springframework.aot.generate.GeneratedTypeReference
+
+@Entity
+data class AvengerEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    @Column(nullable = false)
+    val nick: String,
+    @Column(nullable = false)
+    val person: String,
+    val description: String?,
+    val history: String?
+) {
+    fun toAvenger() = Avenger(id, nick, person, description, history)
+
+    companion object {
+        fun from(avenger: Avenger) = AvengerEntity(
+            id = avenger.id,
+            nick = avenger.nick,
+            description = avenger.description,
+            history = avenger.history,
+            person = avenger.person
+        )
+    }
+}
