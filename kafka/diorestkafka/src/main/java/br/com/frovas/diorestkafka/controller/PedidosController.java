18defedf075246ea9c1ba5580be03abdba6e38ee
frovas89 <frovas@gmail.com>
2024-12-21 08:38:43 -0300

    Iniciando a aplicação

---
+++ kafka/diorestkafka/src/main/java/br/com/frovas/diorestkafka/controller/PedidosController.java
@@ -0,0 +1,14 @@
+package br.com.frovas.diorestkafka.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PedidosController {
+
+    public ResponseEntity<String> salvarPedido() {
+
+
+        return ResponseEntity.ok("Sucesso");
+    }
+}
---
+++ kafka/diorestkafka/src/test/java/br/com/frovas/diorestkafka/TutorialRestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frovas.diorestkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialRestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
